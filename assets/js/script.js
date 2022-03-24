4bd5ba43fa630805759c4b351c25d80bda6146d2
stephaniiekm <stephanie.karlsson.m@gmail.com>
2022-03-24 22:33:47 +0000

    Add functions to the memory-game and starts to add a some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,105 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    //card options
-    const cardArray = [
-        {
-            name: 'angry',
-            img: 'images/angry.png'
-        },
-        {
-            name: 'angry',
-            img: 'images/angry.png'
-        },
-        {
-            name: 'happy',
-            img: 'images/happy.png'
-        },
-        {
-            name: 'happy',
-            img: 'images/happy.png'
-        },
-        {
-            name: 'loving',
-            img: 'images/loving.png'
-        },
-        {
-            name: 'loving',
-            img: 'images/loving.png'
-        },
-        {
-            name: 'sad',
-            img: 'images/sad.png'
-        },
-        {
-            name: 'sad',
-            img: 'images/sad.png'
-        }
-    ];
+//card options
+const cardArray = [
+    {
+        name: 'angry',
+        img: 'assets/images/angry.png'
+    },
+    {
+        name: 'angry',
+        img: 'assets/images/angry.png'
+    },
+    {
+        name: 'happy',
+        img: 'assets/images/happy.png'
+    },
+    {
+        name: 'happy',
+        img: 'assets/images/happy.png'
+    },
+    {
+        name: 'loving',
+        img: 'assets/images/loving.png'
+    },
+    {
+        name: 'loving',
+        img: 'assets/images/loving.png'
+    },
+    {
+        name: 'sad',
+        img: 'assets/images/sad.png'
+    },
+    {
+        name: 'sad',
+        img: 'assets/images/sad.png'
+    }
+];
 
-    const grid = document.querySelector('.grid');
+cardArray.sort(() => 0.5 - Math.random());
+    
+const grid = document.querySelector('.grid');
+const resultDisplay = document.querySelector('#result');
+var cardsChosen = [];
+var cardsChosenId = [];
+var cardsWon = [];
 
 
-    //this function creates the game board
-    //the bord are buildt in cards and creates childrens for the div.grid in the HTML
 
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img');
-            card.setAttribute('src', 'assets/images/loving.png');
-            card.setAttribute('data-id', i);
-            //card.addEventListener('click', flipcard)
-            grid.appendChild(card);
+//this function creates the game board
+//the bord are buildt in cards and creates childrens for the div.grid in the HTML
 
-        }
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img');
+        card.setAttribute('src', 'assets/images/loving.png');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
+        grid.appendChild(card);
+    }
+}
+
+
+//this function checks if the picked cards are matched, if else it flips back and the user gets an alert
+function checkForMatch() {
+    var cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match');
+        cards[optionOneId].setAttribute('src', 'assets/images/blank.png');
+        cards[optionTwoId].setAttribute('src', 'assets/images/blank.png');
+        cardsWon.push(cardsChosen);
+    } else {
+        cards[optionOneId].setAttribute('src', 'assets/images/loving.png');
+        cards[optionTwoId].setAttribute('src', 'assets/images/loving.png');
+        alert('Sorry, try again');
     }
 
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulation! You got them all.';
+    }
+}
+
+
+//this function flip the chosen cards and fill the space with the next image from the cardArray
+//the if-statement checks so two cards are chosen
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500);
+    }
+}
+
+
 createBoard();
-
-
-
-
-
-
-
-
-
-
-
 
 });
